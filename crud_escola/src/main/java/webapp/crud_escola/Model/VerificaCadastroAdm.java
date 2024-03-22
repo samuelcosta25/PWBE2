b8e320165fd2cd5576a4b2cc2063b76d83d6e6c0
samuelcosta25 <samu.live@outlook.com>
2024-03-21 21:54:30 -0300

    21.03.24

---
+++ crud_escola/src/main/java/webapp/crud_escola/Model/VerificaCadastroAdm.java
@@ -0,0 +1,11 @@
+package webapp.crud_escola.Model;
+
+import org.apache.commons.math3.analysis.function.Identity;
+
+Identity 
+public class VerificaCadastroAdm {
+    //atributos
+    @id
+    private String cpf;
+    private String nome;
+}
